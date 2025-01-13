bbaa416e9baf51a71b7e51fee25f7fab2603eb1d
Ethan <trantanthanh29@gmail.com>
2025-01-13 14:51:19 +0700

    chore: remove unused model InvalidatedToken

--- backend/src/main/java/com/example/backend/repository/InvalidatedTokenRepository.java
+++ backend/src/main/java/com/example/backend/repository/InvalidatedTokenRepository.java
@@ -1,6 +1,5 @@
 package com.example.backend.repository;
 
-import com.example.backend.entity.InvalidatedToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
